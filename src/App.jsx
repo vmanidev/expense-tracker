2883ce9ea140b599838754f2c78f20ac513fcd85
vmanidev <vmanidev@outlook.com>
2025-09-07 00:35:36 +0530

    feat: add global styles and css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import PageHeader from "./components/PageHeader/PageHeader";
+import Dashboard from "./features/Dashboard/Dashboard";
+
+import "./app.css";
 
 export default function App() {
   return (
-    <>
+    <div id="app">
       <PageHeader></PageHeader>
-    </>
+      <Dashboard />
+    </div>
   );
 }
