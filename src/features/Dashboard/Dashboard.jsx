9289be86145865ddd7efeae6cdec59f54d4ae79f
vmanidev <vmanidev@outlook.com>
2025-09-07 00:33:07 +0530

    import summary page into dashboard

--- src/features/Dashboard/Dashboard.jsx
+++ src/features/Dashboard/Dashboard.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Summary from "./components/Summary";
 
 export default function Dashboard() {
-  return <div></div>;
+  return (
+    <>
+      <Summary />
+    </>
+  );
 }
