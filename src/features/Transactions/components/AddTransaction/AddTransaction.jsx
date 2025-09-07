61fbcd14ca8e785c631381d9ac122053f21e4d75
vmanidev <vmanidev@outlook.com>
2025-09-07 20:00:30 +0530

    feat: add rupee icon and styling it

--- src/features/Transactions/components/AddTransaction/AddTransaction.jsx
+++ src/features/Transactions/components/AddTransaction/AddTransaction.jsx
@@ -26,34 +26,48 @@ export default function AddTransaction({
       <form>
         <div id="title-field-wrapper">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" placeholder="Title" name="title" />
+          <input
+            type="text"
+            id="title"
+            placeholder="Ex. Shopping"
+            name="title"
+          />
         </div>
         <div id="amount-field-wrapper">
-          <label htmlFor="amount">Amount (&#8377;)</label>
-          <input
-            type="number"
-            id="amount"
-            placeholder="Amount (&#8377;)"
-            name="amount"
-          />
+          <label htmlFor="amount">Amount</label>
+          <div id="amount-input-wrapper">
+            <span className="material-icons material-symbols-outlined">
+              currency_rupee
+            </span>
+            <input
+              type="number"
+              id="amount"
+              placeholder="Ex. (&#8377;) 1000"
+              name="amount"
+            />
+          </div>
         </div>
         <div id="type-field-wrapper">
           <label htmlFor="type">Type</label>
-          <select id="type" placeholder="Type" name="type">
+          <select id="type" placeholder="Income/Expense" name="type">
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
         </div>
         <div id="category-field-wrapper">
           <label htmlFor="category">Category</label>
-          <select id="category" placeholder="Category" name="category">
+          <select
+            id="category"
+            placeholder="Ex.Food, Groceries, etc."
+            name="category"
+          >
             <option>Food</option>
             <option>Groceries</option>
           </select>
         </div>
         <div id="date-field-wrapper">
           <label htmlFor="amount">Date</label>
-          <input type="date" id="date" placeholder="Date" name="date" />
+          <input type="date" id="date" name="date" />
         </div>
       </form>
       <div className="dialog-footer">
