f8c110ab2842452d2c9b16e823b5d1d1927cb3b2
vmanidev <vmanidev@outlook.com>
2025-09-07 18:11:59 +0530

    feat: add input fields and styles for inputs

--- src/features/Transactions/components/AddTransaction/AddTransaction.jsx
+++ src/features/Transactions/components/AddTransaction/AddTransaction.jsx
@@ -23,6 +23,28 @@ export default function AddTransaction({
   return (
     <dialog id="add-transaction-dialog" ref={dialogRef}>
       <h3>Add Transaction</h3>
+      <form>
+        <div id="title-field-wrapper">
+          <label for="title">Title</label>
+          <input id="title" placeholder="Title" name="title" />
+        </div>
+        <div id="amount-field-wrapper">
+          <label for="amount">Amount (&#8377;)</label>
+          <input id="amount" placeholder="Amount (&#8377;)" name="amount" />
+        </div>
+        <div id="type-field-wrapper">
+          <label for="type">Type</label>
+          <input id="type" placeholder="Type" name="type" />
+        </div>
+        <div id="category-field-wrapper">
+          <label for="category">Category</label>
+          <input id="category" placeholder="Category" name="category" />
+        </div>
+        <div id="date-field-wrapper">
+          <label for="amount">Date (&#8377;)</label>
+          <input id="date" placeholder="Date" name="date" />
+        </div>
+      </form>
       <div className="dialog-footer">
         <button className="add-btn">Add</button>
         <button className="cancel-btn" onClick={closeDialog}>
