241e586326fedf3b2b3eb07fb2495ae90be4aea8
vmanidev <vmanidev@outlook.com>
2025-09-07 15:50:38 +0530

    feat: implement transaction list layout and add button

--- src/features/Dashboard/Dashboard.jsx
+++ src/features/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Summary from "./components/Summary";
+import TransactionList from "../Transactions/components/TransactionList";
 
 export default function Dashboard() {
   return (
     <>
       <Summary />
+      <TransactionList />
     </>
   );
 }
