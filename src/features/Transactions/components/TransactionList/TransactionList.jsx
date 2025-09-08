e9210d676ab3a0a3fef0ee0423524104cd0f31fb
vmanidev <vmanidev@outlook.com>
2025-09-09 00:33:20 +0530

    feat: add transaction list

--- src/features/Transactions/components/TransactionList/TransactionList.jsx
+++ src/features/Transactions/components/TransactionList/TransactionList.jsx
@@ -19,11 +19,36 @@ export default function TransactionList() {
     );
   };
 
+  const getExpenseList = (expenseList) => {
+    return expenseList.map(({ id, title, amount, type, category, date }) => {
+      return (
+        <li key={id}>
+          <span id="expense-title">
+            <span
+              className={`${
+                type === "income" ? "income" : "expense"
+              } material-icons material-symbols-outlined`}
+            >
+              {type === "income" ? "call_made" : "call_received"}
+            </span>
+            <span>{title}</span>
+          </span>
+          <span>&#8377;{amount}</span>
+          <span>{type}</span>
+          <span>{category}</span>
+          <span>{date}</span>
+        </li>
+      );
+    });
+  };
+
   return (
     <div>
       <AddTransactionDialog />
       <TransactionListHeader />
-      <div id="transactionList-main-container">{JSON.stringify(expenses)}</div>
+      <div id="transactionList-main-container">
+        <ul>{getExpenseList(expenses)}</ul>
+      </div>
     </div>
   );
 }
