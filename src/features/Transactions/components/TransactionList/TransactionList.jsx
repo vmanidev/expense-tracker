d48838721629ecfd820473474cb0ffc871a88684
vmanidev <vmanidev@outlook.com>
2025-09-08 23:57:09 +0530

    fix: code refactor

--- src/features/Transactions/components/TransactionList/TransactionList.jsx
+++ src/features/Transactions/components/TransactionList/TransactionList.jsx
@@ -6,8 +6,9 @@ import { useExpense } from "../../../../contexts/ExpenseContext";
 
 export default function TransactionList() {
   const { expenses, setOpenDialog } = useExpense();
-  return (
-    <div>
+
+  const TransactionListHeader = () => {
+    return (
       <div id="action-button-container">
         <div className="section-title">Recent Transactions</div>
         <button id="add-btn" onClick={(e) => setOpenDialog(true)}>
@@ -15,7 +16,13 @@ export default function TransactionList() {
           <span>Add</span>
         </button>
       </div>
+    );
+  };
+
+  return (
+    <div>
       <AddTransactionDialog />
+      <TransactionListHeader />
       <div id="transactionList-main-container">{JSON.stringify(expenses)}</div>
     </div>
   );
