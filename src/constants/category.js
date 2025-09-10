4ccd6c03209957ed1393c5de0a0f16f926e7bed6
vmanidev <vmanidev@outlook.com>
2025-09-11 02:34:20 +0530

    feat: add default categories

---
+++ src/constants/category.js
@@ -0,0 +1,31 @@
+export const DEFAULT_CATEGORIES = {
+    income: [
+        "💼 Salary",
+        "📈 Investments",
+        "🏠 Rental",
+        "🎁 Bonuses",
+        "💻 Freelance",
+        "💸 Refunds",
+        "🏆 Awards"
+    ],
+    expenses: [
+        "🍎 Groceries",
+        "🏠 Rent",
+        "🚗 Transport",
+        "💡 Utilities",
+        "🏥 Healthcare",
+        "👕 Clothing",
+        "🍽️ Dining Out",
+        "🎬 Entertainment",
+        "✈️ Travel",
+        "🏋️ Fitness",
+        "🛍️ Shopping",
+        "💳 Loan",
+        "🏦 Savings",
+        "🧾 Taxes",
+        "🎉 Donations",
+        "🐶 Pets",
+        "📚 Education",
+        "⚙️ Miscellaneous"
+    ]
+};
