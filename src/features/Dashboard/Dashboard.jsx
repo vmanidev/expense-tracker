cba9fcfb145dac0a035829b06ab9787925f1c0f7
vmanidev <vmanidev@outlook.com>
2025-09-13 03:50:10 +0530

    feat: render intro or transaction list based on expense count

--- src/features/Dashboard/Dashboard.jsx
+++ src/features/Dashboard/Dashboard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Summary from "./components/Summary/Summary";
 import TransactionList from "../Transactions/components/TransactionList/TransactionList";
+import { useExpense } from "../../contexts/ExpenseContext";
+import Intro from "../../ui-components/Intro/Intro";
 
 export default function Dashboard() {
-  return (
+  const { expenses } = useExpense();
+
+  if (expenses.length < 1) {
+    return <Intro />;
+  } else {
     <>
       <Summary />
       <TransactionList limit="5" />
-    </>
-  );
+    </>;
+  }
 }
