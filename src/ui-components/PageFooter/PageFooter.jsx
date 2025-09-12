4ef4a1d2d578a8d93cf2b23db16b4acca7d427ca
vmanidev <vmanidev@outlook.com>
2025-09-13 03:16:45 +0530

    fix(style): footer layout

--- src/ui-components/PageFooter/PageFooter.jsx
+++ src/ui-components/PageFooter/PageFooter.jsx
@@ -4,12 +4,8 @@ import "./PageFooter.css";
 
 export default function PageFooter() {
   return (
-    <div id="footer-container">
-      <span>Made with ❤️.</span>
-      <span>Open Source under MIT License.</span>
-      <a href="https://github.com/vmanidev/expense-tracker" target="_blank">
-        <img className="icon" src="/images/github.svg" />
-      </a>
-    </div>
+    <footer>
+      &copy; {new Date().getFullYear()} | Expense Manager | MIT License | <a href="https://github.com/vmanidev/expense-tracker">GitHub</a>
+    </footer>
   );
 }
