10ea2b8ed019f34e257a825797da897df936986f
vmanidev <vmanidev@outlook.com>
2025-09-14 03:42:06 +0530

    feat(theme): add theme icon and toggle logic

--- src/ui-components/PageHeader/PageHeader.jsx
+++ src/ui-components/PageHeader/PageHeader.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./pageHeader.css";
 
 export default function PageHeader() {
+  const [theme, setTheme] = useState("dark_mode");
+  const toggleMode = () => {
+    setTheme((prev) => (prev === "light_mode" ? "dark_mode" : "light_mode"));
+  };
+
   return (
     <div id="header-wrapper">
       <span id="page-title">Expense Tracker</span>
-      <span id="settings-icon" className="material-icons material-icons-round">
-        settings
+      <span
+        id="settings-icon"
+        className="material-icons material-symbols-outlined"
+        onClick={toggleMode}
+      >
+        {theme}
       </span>
     </div>
   );
