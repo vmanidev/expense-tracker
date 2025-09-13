247a00866b8a0452b01c31c16e007af7afa072aa
vmanidev <vmanidev@outlook.com>
2025-09-14 00:18:52 +0530

    feat(category): add logic to store and access categories in local storage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,3 +1,7 @@
 export const storeTransactionsLocal = (transactions) => localStorage.setItem("transactions", JSON.stringify(transactions));
 
 export const getLocalTransactions = () => JSON.parse(localStorage.getItem("transactions"));
+
+export const storeCategoriesInLocal = (categories) => localStorage.setItem("categories", JSON.stringify(categories));
+
+export const getLocalCategories = () => JSON.parse(localStorage.getItem("categories"));
