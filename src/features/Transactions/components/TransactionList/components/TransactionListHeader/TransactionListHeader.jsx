56dccb5a6172fe9f09c6ff2e116dc83d6ae0f6d0
vmanidev <vmanidev@outlook.com>
2025-09-13 14:16:25 +0530

    fix(ui): update transaction header title based on URL pathname

--- src/features/Transactions/components/TransactionList/components/TransactionListHeader/TransactionListHeader.jsx
+++ src/features/Transactions/components/TransactionList/components/TransactionListHeader/TransactionListHeader.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import { useExpense } from "../../../../../../contexts/ExpenseContext";
 
 import "./TransactionListHeader.css";
+import { useLocation } from "react-router-dom";
 
 export const TransactionListHeader = () => {
   const { setOpenAddTransactionDialog } = useExpense();
 
+  const { pathname } = useLocation();
+
   return (
     <div id="action-button-container">
-      <div className="section-title">Recent Transactions</div>
+      <div className="section-title">
+        {pathname.includes("transactions")
+          ? "Transactions"
+          : "Recent Transactions"}
+      </div>
       <button id="add-btn" onClick={(e) => setOpenAddTransactionDialog(true)}>
         <span className="material-icons material-symbols-outlined">add</span>
         <span>Add</span>
