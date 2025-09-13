c34f33b5627ef14413ac499657b18ff3ee91ad4d
vmanidev <vmanidev@outlook.com>
2025-09-13 20:32:04 +0530

    feat(category): add category list with styles

--- src/ui-components/Categories/Categories.jsx
+++ src/ui-components/Categories/Categories.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 import "./Categories.css";
+import { useCategory } from "../../contexts/CategoryContext";
 
 export default function Categories() {
-  return <div></div>;
+  const { categories } = useCategory();
+
+  const editBtn = () => (
+    <span className="edit-icon link material-icons material-symbols-outlined">
+      edit
+    </span>
+  );
+
+  const deleteBtn = () => (
+    <span className="delete-icon material-icons material-symbols-outlined">
+      delete_forever
+    </span>
+  );
+
+  const getIncomeCategories = () => {
+    return categories.income.map(({ text, value }) => (
+      <li key={value} value={value}>
+        <span>{text}</span>
+        {editBtn()}
+        {deleteBtn()}
+      </li>
+    ));
+  };
+
+  const getExpenseCategories = () => {
+    return categories.expense.map(({ text, value }) => (
+      <li key={value} value={value}>
+        <span>{text}</span>
+        {editBtn()}
+        {deleteBtn()}
+      </li>
+    ));
+  };
+
+  return (
+    <>
+      <div className="section-title">Categories</div>
+      <div id="category-container">
+        <div className="categories">
+          <span className="section-title">Income</span>
+          <ul>{getIncomeCategories()}</ul>
+        </div>
+        <div className="categories">
+          <span className="section-title">Expenses</span>
+          <ul>{getExpenseCategories()}</ul>
+        </div>
+      </div>
+    </>
+  );
 }
