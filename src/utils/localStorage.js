df51af8480d274582b56d9d5975a7ca575ea17c3
vmanidev <vmanidev@outlook.com>
2025-09-14 22:23:56 +0530

    fix(storage): add try-catch to safely parse localStorage values

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,7 +1,20 @@
-export const storeTransactionsLocal = (transactions) => localStorage.setItem("transactions", JSON.stringify(transactions));
+export const storeTransactionsLocal = (transactions) =>
+    localStorage.setItem("transactions", JSON.stringify(transactions))
 
-export const getLocalTransactions = () => JSON.parse(localStorage.getItem("transactions"));
+export const getLocalTransactions = () =>
+    handleParsingError(localStorage.getItem("transactions"));
 
-export const storeCategoriesInLocal = (categories) => localStorage.setItem("categories", JSON.stringify(categories));
+export const storeCategoriesInLocal = (categories) =>
+    localStorage.setItem("categories", JSON.stringify(categories))
 
-export const getLocalCategories = () => JSON.parse(localStorage.getItem("categories"));
+export const getLocalCategories = () =>
+    handleParsingError(localStorage.getItem("categories"));
+
+const handleParsingError = (data) => {
+    try {
+        const parsedData = JSON.parse(data);
+        return parsedData;
+    } catch (error) {
+        return null;
+    }
+};
