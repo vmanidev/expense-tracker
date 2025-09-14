e06628e7c5f7a6e846d866d9743178d83d17b8d8
vmanidev <vmanidev@outlook.com>
2025-09-15 00:25:49 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,19 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'images/favicon.png',
-            type: 'image/png',
-            sizes: '512x512'
+            "src": "/images/favicon.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          },
+          {
+            "src": "/images/logo192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/images/logo512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
         ]
       }
