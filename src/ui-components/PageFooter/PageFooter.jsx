09cb27ae962a24e5ddf5a9e1c492996e07f5efa6
vmanidev <vmanidev@outlook.com>
2025-09-14 20:29:13 +0530

    refactor(theme): migrate root variables to Material Design standards

--- src/ui-components/PageFooter/PageFooter.jsx
+++ src/ui-components/PageFooter/PageFooter.jsx
@@ -5,7 +5,7 @@ import "./PageFooter.css";
 export default function PageFooter() {
   return (
     <footer>
-      &copy; {new Date().getFullYear()} | Expense Tracker | MIT License | <a href="https://github.com/vmanidev/expense-tracker" target="_blank">GitHub</a>
+      &copy; {new Date().getFullYear()} | Expense Tracker | MIT License | <a className="link" href="https://github.com/vmanidev/expense-tracker" target="_blank">GitHub</a>
     </footer>
   );
 }
--- src/ui-components/PageHeader/PageHeader.jsx
+++ src/ui-components/PageHeader/PageHeader.jsx
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./pageHeader.css";
 
 export default function PageHeader() {
-  const [theme, setTheme] = useState("dark_mode");
-  const toggleMode = () => {
-    setTheme((prev) => (prev === "light_mode" ? "dark_mode" : "light_mode"));
-  };
-
   return (
     <div id="header-wrapper">
       <span id="page-title">Expense Tracker</span>
       <span
-        id="settings-icon"
+        id="toggle-icon"
         className="material-icons material-symbols-outlined"
-        onClick={toggleMode}
       >
-        {theme}
+        dark_mode
       </span>
     </div>
   );
