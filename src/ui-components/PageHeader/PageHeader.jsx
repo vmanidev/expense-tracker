48bf1fbb2134d256a1c44b2a5b5d7feda292f552
vmanidev <vmanidev@outlook.com>
2025-09-14 21:18:43 +0530

    feat: add toggle dark theme logics

--- src/ui-components/PageHeader/PageHeader.jsx
+++ src/ui-components/PageHeader/PageHeader.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./pageHeader.css";
 
 export default function PageHeader() {
+  const [mode, setMode] = useState("light_mode");
+
+  useEffect(() => {
+    if (mode === "dark_mode") document.body.classList.add("dark_mode");
+    else document.body.classList.remove("dark_mode");
+  }, [mode]);
+
   return (
     <div id="header-wrapper">
       <span id="page-title">Expense Tracker</span>
       <span
         id="toggle-icon"
         className="material-icons material-symbols-outlined"
+        onClick={(e) =>
+          setMode((prev) =>
+            prev === "light_mode" ? "dark_mode" : "light_mode"
+          )
+        }
       >
-        dark_mode
+        {mode === "light_mode" ? "dark_mode" : "light_mode"}
       </span>
     </div>
   );
